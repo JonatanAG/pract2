51eede0446cdaacb6e9a22f9cb8bf11236f0795a
usuario <usuario@10.22.143.30>
2015-09-29 10:30:44 +0100

    Version 2

---
+++ src/javaapplication3/CalculaHisto.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author usuario
+ */
+public class CalculaHisto {
+    public static <T> Histogram <T> computeHistogram (T [] vector){
+        
+        Histogram <T> histo = new Histogram<>();
+        
+        for(T key : vector){
+            histo.increment(key);
+        }
+        return histo;
+    }
+}
--- src/javaapplication3/JavaApplication3.java
+++ src/javaapplication3/JavaApplication3.java
@@ -14,6 +14,14 @@ public class JavaApplication3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Integer [] vector = {7,2,3,4,5,6};
+       //String [] vector = {"Ana","Paco","Maria"};
        
+       Histogram <Integer> histogram = CalculaHisto.computeHistogram(vector);
+       for (Object key : histogram.keyset()){
+        System.out.println(key + "-->" + histogram.get(key));
     }
+    }
+    
+    
 }
